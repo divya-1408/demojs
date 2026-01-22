8a6ea9468b997b4b4fbcceacd5324624a985d23b
divya-1408 <divyabharathi1482003@gmail.com>
2026-01-22 11:43:59 +0530

    second update

--- script.js
+++ script.js
@@ -49,3 +49,11 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+const slide = document.querySelectorAll(".slide");
+  let index = 0;
+
+  setInterval(() => {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+  }, 4000);
